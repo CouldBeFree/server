fc8e3e110d881562951da6afc349b44874c574de
CouldBeFree <propokerstar90@gmail.com>
2019-05-01 17:04:21 +0300

    refactored

--- app.js
+++ app.js
@@ -19,16 +19,15 @@ const server = http.createServer((req, res) => {
             console.log(chunk);
             body.push(chunk);
         });
-        req.on('end', () => {
+        return req.on('end', () => {
             const parsedBody = Buffer.concat(body).toString();
             const message = parsedBody.split('=')[1];
-            fs.writeFileSync('message.txt', message);
+            fs.writeFile('message.txt', message, err => {
+                res.statusCode = 302;
+                res.setHeader('Location', '/');
+                return res.end();
+            });
         });
-
-        fs.writeFileSync('message.txt', 'Dummy');
-        res.statusCode = 302;
-        res.setHeader('Location', '/');
-        return res.end();
     }
 
     res.setHeader('Content-Type', 'text/html');
