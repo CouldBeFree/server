355b4fe3d3e3c86c7b4cf9ad0a42276ee859dca1
CouldBeFree <propokerstar90@gmail.com>
2019-05-01 22:05:03 +0300

    added filters

--- routes/shop.js
+++ routes/shop.js
@@ -3,7 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log('Another middleware');
     res.send('<h1>Root route</h1>')
 });
 
