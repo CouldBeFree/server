f466d48f9c4e1e7857116af571ec91273be47716
CouldBeFree <propokerstar90@gmail.com>
2019-05-01 16:35:13 +0300

    get data from client

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     const url = req.url;
+    const method = req.method;
+
     if(url === '/') {
         res.write('<html>');
         res.write('<head><title>Enter message</title></head>');
@@ -9,6 +12,25 @@ const server = http.createServer((req, res) => {
         res.write('</html>');
         return res.end();
     }
+
+    if(url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', chunk => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            fs.writeFileSync('message.txt', message);
+        });
+
+        fs.writeFileSync('message.txt', 'Dummy');
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+    }
+
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Page</title></head>');
